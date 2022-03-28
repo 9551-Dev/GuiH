50cee73ef2a611491bf6a95ccc07b9e4f0123264
9551 <oliverekcaha@gmail.com>
2022-03-28 07:01:01 +0200

    eh

--- object-loader.lua
+++ object-loader.lua
@@ -1,6 +1,6 @@
 return {main=function(object)
     local objects = {}
-    local object_list = fs.list("GuiH/GuiH/objects")
+    local object_list = fs.list("GuiH/objects")
     for k,v in pairs(object_list) do
         local main = require("GuiH.objects."..v..".object")
         objects[v] = function(data)
@@ -8,4 +8,4 @@ return {main=function(object)
         end
     end
     return objects
-end,types=fs.list("GuiH/GuiH/objects")}
+end,types=fs.list("GuiH/objects")}
