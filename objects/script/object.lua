004316b61ba6324174bb00b5deb004d23c8bbef5
9551 <oliverekcaha@gmail.com>
2022-05-08 22:32:18 +0200

    fixed script

--- objects/script/object.lua
+++ objects/script/object.lua
@@ -6,11 +6,23 @@ return function(object,data)
     local base = {
         name=data.name or api.uuid4(),
         visible=data.visible,
+        reactive=data.reactive,
         code=data.code or function() return false end,
         graphic=data.graphic or function() return false end,
         order=data.order or 1,
         logic_order=data.logic_order,
         graphic_order=data.graphic_order,
+        react_to_events={
+            mouse_click=true,
+            mouse_drag=true,
+            monitor_touch=true,
+            mouse_scroll=true,
+            mouse_up=true,
+            key=true,
+            key_up=true,
+            char=true,
+            paste=true
+        }
     }
     return base
 end
