8ffa33d5aaf894f792c0428b57f9522e8121b77b
TotallyNotVirtIO <tobich228@gmail.com>
2022-06-01 16:29:34 +0300

    Added lenghts to the debug prin.
    Signed-off-by: TotallyNotVirtIO <tobich228@gmail.com>

--- graphic_handle.lua
+++ graphic_handle.lua
@@ -439,7 +439,7 @@ local function draw_box_tex(term,tex,x,y,width,height,bg,tg,offsetx,offsety,cach
     --* then we draw the blit data to the screen
     for k,v in pairs(bg_layers) do
         term.setCursorPos(x,y+k-1)
-	local debug_string = ("%s, %s, %s"):format(text_layers[k], fg_layers[k], bg_layers[k])
+	local debug_string = ("%s (%s), %s (%s), %s (%s)"):format(text_layers[k], #text_layers[k], fg_layers[k], #fg_layers[k], bg_layers[k], #bg_layers[k])
 	debugger.print(debug_string)
         term.blit(text_layers[k],fg_layers[k],bg_layers[k])
     end
