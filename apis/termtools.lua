7d4aa7252c281d2e411d9875fd40bf1d74ed31b8
Oliver Caha <74534592+9551-Dev@users.noreply.github.com>
2022-06-16 20:41:22 +0200

    Update termtools.lua

--- apis/termtools.lua
+++ apis/termtools.lua
@@ -4,4 +4,4 @@ s=table.pack(r[i](...))end return table.unpack(s,1,s.n or 1)end end return o
 end local function d(...)local l={...}local u={}for c,m in pairs(l[1])do
 u[c]=function(...)local f={}for w,y in pairs(l)do f=table.pack(y[c](...))end
 return table.unpack(f,1,f.n or 1)end end return u end
-return{mirror_monitors=e,make_shared_terminal=d}
+return{mirror=e,make_shared=d}
