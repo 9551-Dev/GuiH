4b20296e463d1caeb9f700e0f727ea136f1b1e7e
Oliver Caha <74534592+9551-Dev@users.noreply.github.com>
2022-06-22 20:07:13 +0200

    recrafted support ?

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ print("WARNING: standardsMode is set to false, this is not supported by the GuiH
 e=read()if e:lower():match("y")then
 config.set("standardsMode",true)os.reboot()else
 error("GuiH cannot run without standards mode",0)end end end local
-t=fs.getDir(select(2,...))or""local a=package.path
+t=fs.getDir(select(2,...) or "")local a=package.path
 package.path=string.format("%s;/%s/?.lua;/%s/?/init.lua",package.path,t,t)local
 o=require"main"package.path=a return
 setmetatable(o,{__tostring=function()return"GuiH.API"end})
