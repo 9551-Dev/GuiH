bc7dcef9148b5a50d380abce760545b3deb5899c
Oliver Caha <74534592+9551-Dev@users.noreply.github.com>
2022-07-26 17:29:08 +0200

    Update object.lua

--- objects/text/object.lua
+++ objects/text/object.lua
@@ -6,10 +6,11 @@ return function(object,data)
     local base = {
         name=data.name or api.uuid4(),
         visible=data.visible,
-        text=data.text or {text="none",x=1,y=1,offset_x=0,offset_y=0,blit={"0000","eeee"}},
+        text=data.text or object.text{text="none",x=1,y=1,bg=colors.red,fg=colors.white},
         order=data.order or 1,
         logic_order=data.logic_order,
         graphic_order=data.graphic_order,
+        update=data.update or function() end
     }
     return base
 end
