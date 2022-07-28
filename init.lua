f770194a9895931cb9cf1fbc43e1cc760da5d558
Oliver Caha <74534592+9551-Dev@users.noreply.github.com>
2022-07-29 00:28:34 +0200

    it was supported all along

--- init.lua
+++ init.lua
@@ -1,18 +1,3 @@
---* check for emulators
-if config then
-    if config.get("standardsMode") == false then
-        print("WARNING: standardsMode is set to false, this is not supported by the GuiH API")
-        print("Enter Y to enable standards mode, this will reboot the computer")
-        local input = read()
-        if input:lower():match("y") then
-            config.set("standardsMode", true)
-            os.reboot()
-        else
-            error("GuiH cannot run without standards mode",0)
-        end
-    end
-end
-
 --* sets the package path to GuiH's path
 local selfDir = fs.getDir(select(2,...) or "")
 
