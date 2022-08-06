1237c4684323bd40fdbc8674e54e904b747514be
Oliver Caha <74534592+9551-Dev@users.noreply.github.com>
2022-08-06 19:00:17 +0200

    package.path fix

--- init.lua
+++ init.lua
@@ -4,8 +4,8 @@ local selfDir = fs.getDir(select(2,...) or "")
 local old_path = package.path
 
 package.path = string.format(
-    "%s;/%s/?.lua;/%s/?/init.lua",
-    package.path, selfDir,selfDir
+    "/%s/?.lua;/rom/modules/main/?.lua",
+    selfDir
 )
 
 local GuiH = require "main"
